f5540d6382e712a4613c466d5c50721110d2a949
higashi <higashi.y227@bmail.com>
2024-03-26 13:44:30 +0900

    higashi

--- RMsystem/src/main/java/servlet/LoginServlet.java
+++ RMsystem/src/main/java/servlet/LoginServlet.java
@@ -1,40 +1,58 @@
 package servlet;
 
 import java.io.IOException;
+//import java.io.PrintWriter;
 
+import dao.AccountDao;
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import model.AccountBeans;
 
 @WebServlet("/LoginServlet")
 public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	
+	//ログイン画面を表示するためのdoGetメソッド	
 protected void doGet(HttpServletRequest request,
-	HttpServletResponse response)
-	throws ServletException,IOException{
+		HttpServletResponse response)
+		throws ServletException,IOException{
 	
-	RequestDispatcher dispatcher = request.getRequestDispatcher("WEB-INF/jsp/index.jsp");
+	RequestDispatcher dispatcher = request.getRequestDispatcher(
+			"WEB-INF/jsp/index.jsp");
+
 	dispatcher.forward(request, response);
 }
+//ログインフォームから送信されたIDとPASSを取得するためのdoPostメソッド
+protected void doPost(HttpServletRequest request,
+		HttpServletResponse response)
+		throws ServletException,IOException{
+	
+	String id = request.getParameter("id");//リクエストからパラメーターを取得
+    String pass = request.getParameter("pass");
+    
+    
+    
+    
+    // AccountDAOを使用してデータベースに接続し、ログインをチェックする
+    AccountDao accountDAO = new AccountDao();
+    AccountBeans account = accountDAO.findById(id,pass);
 
-protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-    String userId = request.getParameter("name");
-    String password = request.getParameter("pass");
-
-if ("admin".equals(userId) && "password".equals(password)) {
-    // 認証成功時の処理
-    RequestDispatcher dispatcher = request.getRequestDispatcher("WEB-INF/jsp/secretariat.jsp");
-    dispatcher.forward(request, response);
-} else {
-    // 認証失敗時の処理
-    request.setAttribute("errorMessage", "ユーザーIDまたはパスワードが間違っています。");
-    RequestDispatcher dispatcher = request.getRequestDispatcher("WEB-INF/jsp/index.jsp");
-    dispatcher.forward(request, response);
-  }
- }
+    if (account != null) {
+        RequestDispatcher dispatcher = request.getRequestDispatcher(
+                "WEB-INF/jsp/secretariat.jsp");
+        dispatcher.forward(request, response);
+    } else {
+    	 request.setAttribute("errorMessage", "ユーザーIDまたはパスワードが間違っています。");
+    	 RequestDispatcher dispatcher = request.getRequestDispatcher("WEB-INF/jsp/index.jsp");
+    	 dispatcher.forward(request, response);
+    	}
+    }
 }
+
+ 
+
--- RMsystem/src/main/java/servlet/UserokServlet.java
+++ RMsystem/src/main/java/servlet/UserokServlet.java
@@ -2,12 +2,14 @@ package servlet;
 
 import java.io.IOException;
 
+import dao.AccountDao;
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import model.AccountBeans;
 
 @WebServlet("/UserokServlet")
 public class UserokServlet extends HttpServlet {
@@ -15,6 +17,22 @@ public class UserokServlet extends HttpServlet {
     
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+    	
+    	
+    	
+    	
+    	String id = request.getParameter("id");
+        String password = request.getParameter("password");
+         
+      // ユーザー情報をAccountBeansオブジェクトにセット
+         AccountBeans account = new AccountBeans();
+         account.setId(id);
+         account.setPass(password);
+         
+      // AccountDaoクラスのregisterAccountメソッドを呼び出してユーザー情報を登録
+         AccountDao dao = new AccountDao();
+         dao.registerAccount(account);
+         
         
         // ここで必要な処理を行います（例：データベースから備品の情報を取得）
         
